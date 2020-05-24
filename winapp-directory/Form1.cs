a81da1f42490ace51fe295173e1cfa4709a70b0c
tpsumeta <tpsumeta@gmail.com>
2020-05-24 17:47:04 +0700

    Check Directory Exists

--- winapp-directory/Form1.cs
+++ winapp-directory/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,18 @@ namespace winapp_directory
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(@"C:\Temp"))
+            {
+                MessageBox.Show("Directory Exists!", "OK");
+            }
+            else
+            {
+                MessageBox.Show("Directory not Exists!", "Error");
+            }
         }
     }
 }
